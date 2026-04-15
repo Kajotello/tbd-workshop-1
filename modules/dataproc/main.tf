35afda9839eb4faa273306619162bf1a92da8fb8
kajotello <kajotello@gmail.com>
2026-04-15 15:58:21 +0200

    added spot instances

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -132,7 +132,13 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
         boot_disk_type    = "pd-standard"
         boot_disk_size_gb = 100
       }
-
     }
+
+    secondary_worker_config {
+      num_instances = 4
+      machine_type  = var.machine_type
+      preemptibility = "SPOT" 
+    }
+
   }
 }
